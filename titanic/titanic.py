1f19bcc1a10209cc2614013ec748b1db18172511
최종규 <153177760+jonggu12@users.noreply.github.com>
2024-03-21 23:16:14 +0900

    Add files via upload

---
+++ titanic/titanic.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from mangum import Mangum
+import joblib
+import pandas as pd
+
+app = FastAPI()
+handler = Mangum(app)
+
+# 저장된 모델 로드
+model = joblib.load("titanic/titanic_model.joblib")
+
+class Passenger(BaseModel):
+    Pclass: int
+    Sex: str
+    Age: float
+    SibSp: int
+    Parch: int
+    Fare: float
+
+@app.post("/predict-survival")
+async def predict_survival(passenger: Passenger):
+    passenger_data = pd.DataFrame([passenger.dict()])
+    prediction = model.predict(passenger_data)
+    return {"survival_prediction": int(prediction[0])}
